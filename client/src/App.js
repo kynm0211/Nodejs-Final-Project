d170bb08d9281be69257f703ca06f9025b728813
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-10-11 20:01:24 +0700

    short code

--- client/src/App.js
+++ client/src/App.js
@@ -10,55 +10,47 @@ import axios from 'axios';
 import {isAuthURL} from './middlewares/requiredLogin';
 function App() {
 
-  // Initial Authentication State
-  const [user, setUser] = useState(null);
-  const [navigator, setNavigator] = useState(false);
-  function fetchUserData(token) {
-    axios.get('/api/current_user', {
-        headers: {
-          'Authorization': `${token}`
-        }
-    })
-    .then(response => {
-        const userData = response.data;
-        console.log('User data:', userData);
-        setUser({...userData});
-    })
-    .catch(error => {
-        console.error('Error fetching user data', error);
-    });
-  }
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if(token) {
-        fetchUserData(token);
-    }else{
-		setNavigator(true);
+	// Initial Authentication State
+	const [user, setUser] = useState(null);
+	const [navigator, setNavigator] = useState(false);
+
+
+	function fetchUserData(token) {
+		axios.get('/api/current_user', {headers: {'Authorization': `${token}`}})
+			.then(response => {setUser({...response.data});})
+			.catch(error => {console.error('Error fetching user data', error);
+		});
 	}
-	
-	if(isAuthURL() === false && !token){
-		setNavigator(true);
-	}else{
-		setNavigator(false);
-	}
-  }, []);
+  	useEffect(() => {
+		// Check if a token exists in local storage
+		const token = localStorage.getItem('token');
+		
+		if (token) {
+			fetchUserData(token);
+		} else {
+			// If no token is found, set the navigator to true
+			setNavigator(true);
+		}
+
+		// Check if the current URL is not authenticated and there is no token
+		setNavigator(isAuthURL() === false && !token? true : false);
+	}, []);
 
 
-
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/*" element={<Header user={user}/>} />
-          <Route path="/login" element={<LoginRegister/>} />
-          <Route path="/forget" element={<ForgetPassword />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
-	  	{navigator ? <Navigate to="/login" /> : null}
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+		<Router>
+			<Routes>
+			<Route path="/*" element={<Header user={user}/>} />
+			<Route path="/login" element={<LoginRegister/>} />
+			<Route path="/forget" element={<ForgetPassword />} />
+			<Route path="/upload" element={<Upload />} />
+			<Route path="/logout" element={<Logout />} />
+			</Routes>
+			{navigator ? <Navigate to="/login" /> : null}
+		</Router>
+		</div>
+	);
 }
 
 export default App;
