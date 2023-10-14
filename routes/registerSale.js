4fad37693a1667afaa985e3ed0d0186641d7e538
Hong Kiet <36097702+hongkiet@users.noreply.github.com>
2023-10-14 17:42:48 +0700

    Update
    Send Mail

--- routes/registerSale.js
+++ routes/registerSale.js
@@ -4,28 +4,30 @@ const package = require('../middlewares/package');
 const hashPassword = require('../service/hash256');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const jwt = require('jsonwebtoken');
+const sendEmail = require('../service/gmailSender');
+
 module.exports = (app) => {
     app.use(bodyParser.urlencoded({ extended: false }));
 
     app.post('/api/admin/create-account-sale', async (req, res) => {
         const password = "1234567";
         const { name, email, role } = req.body;
-        console.log(name, email, role);
-
+    
         // Kiểm tra dữ liệu gửi từ phía máy khách
         if (!name || !email || !role) {
             return res.json(package(1, "Missing required fields", null));
         }
-
+    
         // Kiểm tra email đã tồn tại trong cơ sở dữ liệu
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.json(package(9, "Email already exists", null));
         }
-
+    
         // Hash mật khẩu trước khi lưu vào cơ sở dữ liệu
         const hashedPassword = hashPassword(password, KEY.SECRET_SALT);
-
+    
         // Tạo một đối tượng User mới và lưu vào cơ sở dữ liệu
         const newUser = new User({
             name: name,
@@ -35,22 +37,25 @@ module.exports = (app) => {
             role: role,
             status: "InActive",
         });
-
+    
         try {
             await newUser.save();
-            
-            // Tạo một mã thông báo đăng nhập (có thể sử dụng mã ngẫu nhiên)
-            // const loginToken = ;
-
+    
+            // Tạo một token JWT cho người dùng đã đăng ký
+            const token = jwt.sign({ userId: newUser._id }, KEY.SECRET_SESSION_KEY, { expiresIn: '24h' });
+    
             // Tạo liên kết đăng nhập
-            // const loginLink = `http://localhost:3000/login?token=${loginToken}`;
-
-            // Gửi email thông báo với liên kết đăng nhập
-            // await axios.post('/api/admin/send-login-link', { toEmail: email, loginLink });
-            
+            const loginLink = `http://localhost:3000/login?token=${token}`;
+            console.log(loginLink);
+            console.log(email);
+            // Gửi email với liên kết đăng nhập
+            sendEmail(email, "Login Link", `Click the following link to log in: ${loginLink}`);
+    
             return res.json(package(0, "Registration success", newUser));
         } catch (error) {
+            console.error(error);
             return res.json(package(11, "Internal error", error));
         }
     });
+    
 };
