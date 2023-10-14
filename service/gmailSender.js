da8fce4ebe473251b187a64d5270f2620632f52d
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-10-14 18:10:59 +0700

    fix sender

--- service/gmailSender.js
+++ service/gmailSender.js
@@ -2,31 +2,28 @@ require('dotenv').config(); // Nạp biến môi trường từ tệp .env
 
 const nodemailer = require("nodemailer");
 
-module.exports = (app) => {
-    const transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: process.env.EMAIL_USER, // Sử dụng biến môi trường
-            pass: process.env.EMAIL_PASSWORD, // Sử dụng biến môi trường
-        },
-    });
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.EMAIL_USER, // Sử dụng biến môi trường
+        pass: process.env.EMAIL_PASSWORD, // Sử dụng biến môi trường
+    },
+});
 
-    const sendEmail = (toEmail, subject, text) => {
-        const mailOptions = {
-            from: "Service",
-            to: toEmail,
-            subject: subject,
-            text: text,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Email sent: " + info.response);
-            }
-        });
+sendEmail = (toEmail, subject, text) => {
+    const mailOptions = {
+        from: "Service",
+        to: toEmail,
+        subject: subject,
+        text: text,
     };
 
-    app.sendEmail = sendEmail;
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
+module.exports = sendEmail;
