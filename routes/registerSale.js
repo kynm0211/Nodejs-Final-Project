63b17e30d88223a387fbe97af820d5e65703b4ab
Hong Kiet <36097702+hongkiet@users.noreply.github.com>
2023-10-14 19:50:44 +0700

    Update
    FrontEnd for Create Sale

--- routes/registerSale.js
+++ routes/registerSale.js
@@ -8,27 +8,25 @@ const jwt = require('jsonwebtoken');
 const sendEmail = require('../service/gmailSender');
 
 module.exports = (app) => {
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json()); // Sử dụng body-parser
 
     app.post('/api/admin/create-account-sale', async (req, res) => {
         const password = "1234567";
-        const { name, email, role } = req.body;
-    
-        // Kiểm tra dữ liệu gửi từ phía máy khách
+        const name = req.body.name;
+        const email = req.body.email;
+        const role = req.body.role;
+
         if (!name || !email || !role) {
             return res.json(package(1, "Missing required fields", null));
         }
-    
-        // Kiểm tra email đã tồn tại trong cơ sở dữ liệu
+
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.json(package(9, "Email already exists", null));
         }
-    
-        // Hash mật khẩu trước khi lưu vào cơ sở dữ liệu
+
         const hashedPassword = hashPassword(password, KEY.SECRET_SALT);
-    
-        // Tạo một đối tượng User mới và lưu vào cơ sở dữ liệu
+
         const newUser = new User({
             name: name,
             email: email,
@@ -37,25 +35,22 @@ module.exports = (app) => {
             role: role,
             status: "InActive",
         });
-    
+
         try {
             await newUser.save();
-    
-            // Tạo một token JWT cho người dùng đã đăng ký
+
             const token = jwt.sign({ userId: newUser._id }, KEY.SECRET_SESSION_KEY, { expiresIn: '24h' });
-    
-            // Tạo liên kết đăng nhập
-            const loginLink = `http://localhost:3000/login?token=${token}`;
+
+            const loginLink = `http://localhost:3000/direct?token=${token}`;
             console.log(loginLink);
             console.log(email);
-            // Gửi email với liên kết đăng nhập
+
             sendEmail(email, "Login Link", `Click the following link to log in: ${loginLink}`);
-    
+
             return res.json(package(0, "Registration success", newUser));
         } catch (error) {
             console.error(error);
             return res.json(package(11, "Internal error", error));
         }
     });
-    
 };
