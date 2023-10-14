f471cde43dc7fca3c37084fc1bff51039d36f8a0
Hong Kiet <36097702+hongkiet@users.noreply.github.com>
2023-10-14 16:28:05 +0700

    Add
    Some bugs...

--- service/gmailSender.js
+++ service/gmailSender.js
@@ -1,41 +1,32 @@
+require('dotenv').config(); // Nạp biến môi trường từ tệp .env
+
 const nodemailer = require("nodemailer");
 
 module.exports = (app) => {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: "eservice240@gmail.com", 
-      pass: "matkhau123", 
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: process.env.EMAIL_USER, // Sử dụng biến môi trường
+            pass: process.env.EMAIL_PASSWORD, // Sử dụng biến môi trường
+        },
+    });
 
+    const sendEmail = (toEmail, subject, text) => {
+        const mailOptions = {
+            from: "Service",
+            to: toEmail,
+            subject: subject,
+            text: text,
+        };
 
-  const sendEmail = (toEmail, subject, text) => {
-    const mailOptions = {
-      from: "Service", 
-      to: toEmail,
-      subject: subject,
-      text: text,
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email sent: " + info.response);
+            }
+        });
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-  };
-
-  // Gửi email với liên kết đăng nhập
-  app.post("/api/admin/send-login-link", (req, res) => {
-    const { toEmail, loginLink } = req.body;
-
-    const subject = "Welcome to Our System";
-    const text = `Welcome to our system! Please click on the following link to set your password and log in: ${loginLink}`;
-
-    sendEmail(toEmail, subject, text);
-
-    return res.json({ message: "Email sent successfully" });
-  });
+    app.sendEmail = sendEmail;
 };
