6c84849381373d70d9b787a5653a0563227dbc26
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-11-12 23:09:56 +0700

    update login with customer

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -5,7 +5,7 @@ const hashPassword = require('../service/hash256');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-
+const Customer = mongoose.model('Customer');
 
 module.exports = (app) =>{
     app.use(bodyParser.json());
@@ -35,37 +35,51 @@ module.exports = (app) =>{
             username: username,
             password: hashedPassword,
         }
-
         try {
             const userDB = await User.findOne({ username });
-        
-            if (!userDB) {
+            const customerDB = await Customer.findOne({ phone: username });
+            
+            if (!userDB && !customerDB) {
                 return res.json(package(10, "Invalid username or password", null));
-              
+                
             }
         
             
-            if (user.password === userDB.password) {
-                // Store user information in the session
-                const userWithoutPassword = { ...userDB };
-                delete userWithoutPassword._doc.password;
-
-                prepairUser = {...userWithoutPassword};
-                prepairUser._doc.token = jwt.sign(prepairUser._doc, KEY.SECRET_SESSION_KEY , { expiresIn: '24h' });
-                if(prepairUser._doc.status === 'InActive'){
-                    return res.json(package(12, "Your account is not active", null));
-                }else if(prepairUser._doc.status === 'Lock'){
-                    return res.json(package(12, "Your account is locked by Administator", null));
+            if(userDB){
+                if (user.password === userDB.password) {
+                    // Store user information in the session
+                    const userWithoutPassword = { ...userDB };
+                    delete userWithoutPassword._doc.password;
+    
+                    prepairUser = {...userWithoutPassword};
+                    prepairUser._doc.token = jwt.sign(prepairUser._doc, KEY.SECRET_SESSION_KEY , { expiresIn: '24h' });
+                    if(prepairUser._doc.status === 'InActive'){
+                        return res.json(package(12, "Your account is not active", null));
+                    }else if(prepairUser._doc.status === 'Lock'){
+                        return res.json(package(12, "Your account is locked by Administator", null));
+                    }
+    
+    
+                    return res.json(
+                        package(0, "Login success", prepairUser._doc)
+                    );
+                    
+                } else {
+                    return res.json(package(10, "Invalid username or password", null));
+                    
                 }
-
-
-                return res.json(
-                    package(0, "Login success", prepairUser._doc)
-                );
-                
-            } else {
-                return res.json(package(10, "Invalid username or password", null));
-                
+            }else if(customerDB){
+                if(user.password === customerDB.password){
+                    const userWithoutPassword = { ...customerDB };
+                    delete userWithoutPassword._doc.password;
+    
+                    prepairUser = {...userWithoutPassword};
+                    prepairUser._doc.token = jwt.sign(prepairUser._doc, KEY.SECRET_SESSION_KEY , { expiresIn: '24h' });
+    
+                    return res.json(
+                        package(0, "Login success", prepairUser._doc)
+                    );
+                }
             }
         } catch (error) {
             return res.json(package(11, "Internal error", error.message));
