0ac0ea755d42d093801e8cd3dff469d68a43b6e5
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-12-07 13:30:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.use('/api/pos', require('./Routers/POSRoute'));
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use("/", express.static(path.join(__dirname, "client", "build")));
     const path = require('path');
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
