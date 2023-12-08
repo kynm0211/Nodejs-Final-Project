ecbde5f5c68b53def3fb283dcf9f219c8829e4f6
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-12-09 00:46:32 +0700

    Update gmailSender.js

--- service/gmailSender.js
+++ service/gmailSender.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: "gmail",
     host:"smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
